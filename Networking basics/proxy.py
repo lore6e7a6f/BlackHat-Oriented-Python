887453fc8d0d2720978d8094ee60b6a09cc1796b
lore6e7a6f <lorenzo.totis90@gmail.com>
2025-12-07 20:13:59 +0100

    modified:   Networking basics/proxy.py

--- Networking basics/proxy.py
+++ Networking basics/proxy.py
@@ -76,4 +76,61 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
             hex_dump(remote_buffer)
             
             # remote_buffer = response_handler(remote_buffer) not usefull yet
-    
+            remote_socket.send(remote_buffer)
+            print("[==>] Sent to localhost.")
+
+        if not len(local_buffer) or not len(remote_buffer):
+            client_socket.close()
+            remote_socket.close()
+            print("[!] No more data, closing connections...")
+            break
+
+def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        server.bind((local_host, local_port))
+    except Exception as e:
+        print('Problem on bind: %o' %e)
+        print('[!] Failed to listen on %s:%d' %(local_host, local_port))
+        sys.exit()
+
+    print('[*] Listening on %s:%d' %(local_host, local_port))
+    server.listen(5)
+
+    while True:
+        client_socket, addr = server.accept()
+        line = "> Received incoming connection from %s:%d" (addr[0], addr[1])
+        print(line)
+
+        proxy_thread = threading.Thread(
+            target=proxy_handler,
+            args=(client_socket, remote_host, remote_port, receive_first)
+        )
+
+        proxy_thread.start()
+
+def main():
+    if len(sys.argv[1:] != 5):
+        print("Usage: ,/proxy.py [localhost] [localport]", end='')
+        print("[remotehost] [remoteport] [receive_first]")
+        print("Example: ./proxy.py 127.0.0.1 9000 10.12.132.1 9000 True")
+        sys.exit()
+
+    local_host = sys.argv[1]
+    local_port = int(sys.argv[2])
+
+    remote_host = sys.argv[3]
+    remote_port = int(sys.argv[4])
+
+    receive_first = sys.argv[5]
+    if True in recieve_first:
+        recieve_first = True
+    else:
+        recieve_first = False
+
+    server_loop(local_host, local_port, remote_host, remote_port, receive_first)
+
+if __name__ == "__main__":
+    main()
+
+
